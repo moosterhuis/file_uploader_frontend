21ebfde4c9c805fb61098a8cbe077356de8576e2
MOOSTERH <Qwerty@007>
2022-11-11 13:43:10 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { BrowserModule } from
+	'@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FileUploadComponent } from
+	'./file-upload/file-upload.component';
+
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from
+	'@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+	AppComponent,
+	FileUploadComponent,
+],
+imports: [
+	BrowserModule,
+	HttpClientModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
